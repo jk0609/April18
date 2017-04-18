80e1be7263c8338a02e6a73cb52d657f4095ead5
Max Reichman <reichmanmax@gmail.com>
2017-04-18 16:34:47 -0700

    most of words to numbers

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -50,3 +50,35 @@ class Fixnum
     answer_arr.join(" ")
   end
 end
+
+class String
+  define_method(:words_to_numbers) do
+    position = {
+      4 => ' trillion', 3 => ' billion', 2=> ' million', 1=> ' thousand', 0=> ''
+    }
+    numbers = {
+      1000000000000=>'trillion',1000000000=>'billion', 1000000=>'million',1000=>'thousand',100=>'hundred', 900 => 'nine hundred', 800 => 'eight hundred', 700 => 'seven hundred', 600 => 'six hundred', 500 => 'five hundred', 400 => 'four hundred', 300 => 'three hundred', 200 => 'two hundred', 100 => 'hundred', 90 => 'ninety', 80 => 'eighty', 70 => 'seventy', 60 => 'sixty', 50 => 'fifty', 40 => 'forty', 30 => 'thirty', 20 => 'twenty', 19 => 'nineteen', 18 => 'eighteen', 17 => 'seventeen', 16 => 'sixteen', 15 => 'fifteen', 14 => 'fourteen', 13 => 'thirteen', 12 => 'twelve', 11 => 'eleven', 10=> 'ten', 9 => 'nine', 8 => 'eight', 7 => 'seven', 6 => 'six', 5 => 'five', 4 => 'four', 3 => 'three', 2 => 'two', 1=> 'one'
+    }
+
+    string_array = []
+    answer = []
+    breakpoint = -1
+    self.split(" ").each_with_index() do |string, i|
+      if position.invert().include?(" "+string)
+        string_array.push(self.split(" ")[breakpoint+1..i])
+        breakpoint = i
+      end
+    end
+    string_array.push(self.split(" ")[breakpoint+1..self.split(" ").length-1])
+
+    string_array.each() do |arr|
+      arr.each_with_index() do |string, i|
+        arr[i] = numbers.invert().fetch(string)
+      end
+    end
+    string_array[string_array.length-1].push(1)
+
+    #for each entry of string_array, multiply (all values EXCEPT last entry)*(last entry)
+    #add all final totals of string array
+  end
+end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -8,7 +8,13 @@ describe('Fixnum#numbers_to_words') do
   # it('successfully returns a two digit number') do
   #   expect(13.numbers_to_words()).to(eq('thirteen'))
   # end
+  # it('successfully returns a three digit number') do
+  #   expect(123456789.numbers_to_words()).to(eq('one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine'))
+  # end
+end
+describe('String#words_to_numbers') do
+
   it('successfully returns a three digit number') do
-    expect(123456789.numbers_to_words()).to(eq('one hundred twenty three million four hundred fifty six thousand seven hundred eighty nine'))
+    expect('six million five hundred fifty thousand five hundred fifty two'.words_to_numbers()).to(eq(6550552))
   end
 end
